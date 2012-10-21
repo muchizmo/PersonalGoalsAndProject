576060de913124ff0324309b20da09a64cd5e6ca
muchizmo <muchizmo@gmail.com>
2012-10-22 00:41:35 +0600

    Reloading page after updating. App name changed. Icons for jQueryMobile added.

--- src/muchizmo/projectsTracker/ProjectsTrackerActivity.java
+++ src/muchizmo/projectsTracker/ProjectsTrackerActivity.java
@@ -1,13 +1,14 @@
-package your.application.sampleApplication;
+package muchizmo.projectsTracker;
 
 import org.apache.cordova.DroidGap;
 import android.os.Bundle;
 
-public class SampleApplicationActivity extends DroidGap {
+public class ProjectsTrackerActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        super.setIntegerProperty("loadUrlTimeoutValue", 600000);
     }
 }
